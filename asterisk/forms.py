0e3ccf5e5bf86e70403ba4f2dfe0b87c4a2807af
ahmednamoha <ahmedmohamedbt6@gmail.com>
2020-09-09 10:08:34 +0300

    mult model one form submit

--- asterisk/forms.py
+++ asterisk/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 
 from django.forms import ModelForm, TextInput, FileField, NumberInput
-from .models import Extentions
+from .models import Extentions, Queue
 
 
 class ExtentionsForm(ModelForm):
@@ -12,3 +12,12 @@ class ExtentionsForm(ModelForm):
         fields = ['exten', 'file']
         widgets = {'exten': NumberInput(
             attrs={'class': 'form-control', 'placeholder': 'Short code'})}
+
+
+class QueueForm(ModelForm):
+    class Meta:
+        model = Queue
+        fields = ['name', 'optin', 'exten']
+        widgets = {'optin': NumberInput(
+            attrs={'class': 'form-control', 'placeholder': '1'}), 'name': TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'queue name'})}
--- asterisk/views.py
+++ asterisk/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 # from django.http import HttpResponse, HttpResponseRedirect
 from .models import Extentions
-from .forms import ExtentionsForm
+from .forms import ExtentionsForm, QueueForm
 from django import forms
 from django.contrib import messages
 
@@ -9,14 +9,28 @@ from django.contrib import messages
 def index(request):
     if request.method == 'POST':
         form = ExtentionsForm(request.POST, request.FILES)
+
         if form.is_valid():
+            form2data = {}
+            form2data['name'] = request.POST['name']
+            form2data['exten'] = request.POST['exten']
+            form2data['optin'] = request.POST['optin']
+
+            form2 = QueueForm(form2data)
+            if form2.is_valid():
+
+                form2.save()
             form.save()
             print('-_'*100)
             print(form.is_valid())
+            form.cleaned_data['exten']
+            form.cleaned_data['file']
             print(form.errors)
+            messages.success(
+                request, ' successful Registered ')
             # return render(request, 'base.html', {'name': 'ahmed', 'form': form})
-            rul = 1
-            return redirect('/')
+            # rul = 1
+            # return redirect('/')
 
         else:
             print('*'*100)
@@ -34,8 +48,12 @@ def index(request):
         form = ExtentionsForm()
 
     form = ExtentionsForm()
+    form2 = QueueForm()
+    formall = {}
+    formall['form'] = form
+    formall['form2'] = form2
 
-    return render(request, 'base.html', {'name': 'ahmed', 'form': form})
+    return render(request, 'base.html', formall)
 
 # Create your views here.
 
